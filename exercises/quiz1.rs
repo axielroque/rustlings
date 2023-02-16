a9f24e19fbb26c4d175fa9c9ff2ffd4ef29a6d5d
Axiel Roque <axiel.roque@outlook.es>
2023-02-16 10:20:18 -0600

    Completed: exercises/quiz1

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,11 +10,16 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
-
+fn calculate_price_of_apples(quantity: i32) -> i32 {
+    let mut cost: i32;
+    if quantity <= 40 {
+        cost = quantity * 2;
+    } else {
+        cost = quantity * 1;
+    }
+    cost
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
